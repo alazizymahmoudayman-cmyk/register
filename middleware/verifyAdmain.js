ed29e49bfc5b2f7314eb23b9c57e06d435430592
alazizymahmoudayman-cmyk <alazizymahmoudayman@gmail.com>
2026-04-26 22:36:36 +0300

    fix Admain

--- middleware/verifyAdmain.js
+++ middleware/verifyAdmain.js
@@ -1,9 +1,8 @@
 const verifyAdmin = (req, res, next) => {
-    // لو الـ role مش Admin يرفض الطلب
     if (req.role !== 'Admin') {
         return res.status(403).json({ message: 'صلاحيات أدمن فقط!' });
     }
-    next(); // لو أدمن يخليه يكمل للدالة اللي بعدها
+    next();
 };
 
 module.exports = verifyAdmin;
